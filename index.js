e1efbd46ff5a3a4a644ab87fa24d055be6b2f840
yorkie <yorkiefixer@gmail.com>
2015-08-05 06:02:56 +0800

    v1.1.4 and fix bug

--- index.js
+++ index.js
@@ -1,192 +1,37 @@
 
-/**
- * @method appendModules
- */
-function appendModules (list) {
-  for (var name in this.ast.modules) {
-    var mod = this.ast.modules[name];
-    if (!mod || mod.type !== 'modules') { 
-      continue;
+var Intellisense = function (ast) {
+  var nsMap = ast.namespacesMap;
+  var classesKeys = Object.keys(ast.classes);
+  var modulesKeys = Object.keys(ast.modules);
+  var ret = classesKeys.concat(modulesKeys);
+  ast.members.forEach(function (member) {
+    var parent = member.parent ||
+      ast.classes[member.clazz] ||
+      ast.modules[member.module];
+    if (!parent.members || parent.members.length === 0) {
+      parent.members = {};
     }
-    mod.members = mod.members || [];
-    mod.classes = mod.classes || {};
-    mod.submodules = mod.submodules || {};
-    if (mod.namespace) {
-      this.namespaces[mod.namespace] = mod;
-    }
-    if (mod.name.indexOf('.') !== -1) {
-      this.submodules[mod.name] = mod;
-    }
-    for (var subClassName in mod.classes) {
-      if (this.ast.classes[subClassName] === null)
-        this.ast.classes[subClassName] = mod.classes[subClassName];
-    }
-    for (var submoduleName in mod.submodules) {
-      var submod = this.ast.modules[submoduleName];
-      if (!submod) {
-        submod = this.ast.modules[submoduleName] = mod.submodules[submoduleName];
-        this.namespaces[submoduleName] = submod;
-      }
-    }
-    list.push({
-      'text': name,
-      'type': 'module',
-      'description': mod.description
-    });
-  }
-  return list;
-}
-
-/**
- * @method appendSubmodules
- */
-function appendSubmodules (list) {
-  for (var src in this.submodules) {
-    var sp = src.lastIndexOf('.');
-    var ns = src.slice(0, sp);
-    var name = src.slice(sp + 1);
-    var parent = this.namespaces[ns];
-    if (parent) {
-      parent.submodules[name] = this.submodules[src];
-    }
-  }
-  return list;
-}
-
-/**
- * @method appendClasses
- */
-function appendClasses (list) {
-  for (var name in this.ast.classes) {
-    var clazz = this.ast.classes[name];
-    if (clazz) {
-      clazz.members = clazz.members || [];
-      this.namespaces[clazz.namespace] = clazz;
-      var mod = this.namespaces[clazz.module];
-      if (mod && mod.classes) {
-        mod.classes[clazz.name] = clazz;
-      }
-      list.push({
-        'text': name,
-        'type': 'class',
-        'description': clazz.description
-      });
-    }
-  }
-  return list;
-}
-
-/**
- * @method appendMembers
- */
-function appendMembers (list) {
-  for (var idx in this.ast.members) {
-    var member = this.ast.members[idx];
-    if (member) {
-      var ns = member.namespace;
-      var parentNS = [member.module, member.clazz].filter(
-        function (item) { return item; }
-      ).join('.');
-      var parent = this.namespaces[parentNS];
-      if (parent && parent.members) {
-        parent.members.push(member);
-      }
-      if (member.itemtype === 'method') {
-        if (member.module === ns) {
-          ns = ns + '.__constructor__';
+    if (member.itemtype === 'method') {
+      parent.members[member.name] = function () {};
+    } else if (member.itemtype === 'property') {
+      try {
+        var type = member.type.replace(/[\{\}]/g, '').split('.').pop();
+        var isArray = /\[\]$/.test(type);
+        if (isArray) {
+          parent.members[member.name] = [];
+        } else {
+          parent.members[member.name] = type;
         }
-        this.namespaces[ns] = member;
-      } else {
-        this.namespaces[ns] = member;
-      }
+      } catch (e) {}
     }
-  }
-  return list;
-}
-
-/**
- * @method getNext
- */
-function getNext (root) {
-  var next = {};
-  var name;
-  for (name in root.classes || {}) {
-    var clazz = root.classes[name];
-    next[name] = {
-      'text': name,
-      'type': 'class',
-      'description': clazz.description
-    };
-  }
-  for (name in root.submodules || {}) {
-    var mod = root.submodules[name];
-    next[name] = {
-      'text': name,
-      'type': 'module',
-      'description': mod.description
-    };
-  }
-  for (var idx in root.members || []) {
-    var member = root.members[idx];
-    var typeDisplay = member.itemtype || 'member';
-    if (typeDisplay === 'method') {
-      var args = (member.params || []).map(function (arg) {
-        return arg.name + ': ' + arg.type;
-      });
-      typeDisplay = '(' + args.join(', ') + ')';
-      if (member.return && member.return.type) {
-        typeDisplay += ' => ' + member.return.type + '';
-      }
-    } else if (typeDisplay === 'property') {
-      typeDisplay += (': ' + (member.type || 'any'));
-    }
-    next[member.name] = {
-      'text': member.name,
-      'type': typeDisplay,
-      'description': member.description
-    };
-  }
-  return next;
-}
-
-/**
- * @method ListPrototypeGet
- */
-function ListPrototypeGet (name) {
-  if (/\[\]$/.test(name)) {
-    return { 'next': [] };
-  }
-  var ret = this.namespaces[name] || 
-    this.ast.classes[name];
-  if (ret) {
-    var type = this.ast.classes[ret.type];
-    if (type) {
-      ret.next = getNext(type);
-    } else {
-      ret.next = getNext(ret);
-    }
-  }
+  });
+  ret.get = function (name) {
+    return ast.classes[name] || ast.modules[name];
+  };
+  ret.getByNs = function (name) {
+    return nsMap[name];
+  };
   return ret;
 };
-
-/**
- * @method Intellisense
- * @export
- */
-function Intellisense (ast) {
-  var list = [];
-  var ctx = {
-    ast: ast,
-    namespaces: {},
-    submodules: {}
-  };
-  list = appendModules.call(ctx, list);
-  list = appendSubmodules.call(ctx, list);
-  list = appendClasses.call(ctx, list);
-  list = appendMembers.call(ctx, list);
-  list.get = ListPrototypeGet.bind(ctx);
-  console.log(ctx.namespaces);
-  return list;
-}
 
 module.exports = Intellisense;
